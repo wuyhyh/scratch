7588e1678208c75de15552eeaefc33fccad54645
wuyh <wuyhyh@gmail.com>
2024-03-22 00:22:23 +0800

    unix taste

---
+++ Advance_unix/ls.c
@@ -0,0 +1,23 @@
+//
+// Created by wyh on 2024/3/22.
+//
+
+// ls命令的实现
+
+#include "apue.h"
+#include <dirent.h>
+
+int main(int argc, char *argv[])
+{
+	DIR *dp;
+	struct dirent *dirp;
+	if (argc != 2)
+		err_quit("usage is not a directory_name");
+
+	if ((dp = opendir(argv[1])) == NULL)
+		err_sys("can't open %s", argv[1]);
+	while ((dir = readdir(dp)) != NULL)
+		printf("%s\n", dirp->d_name);
+	closedir();
+	exit(0);
+}
