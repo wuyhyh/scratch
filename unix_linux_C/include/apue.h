0537a2fb9f5578957ea1ccee9ee468aab9535800
wuyh <wuyhyh@gmail.com>
2024-04-24 00:33:39 +0800

    fix apue header

--- unix_linux_C/include/apue.h
+++ unix_linux_C/include/apue.h
@@ -58,7 +58,55 @@ ssize_t writen(int, const void *, size_t);
 
 int fd_pipe(int *);
 
+// 锁
+int lock_reg(int, int, int, off_t, int, off_t);
+
+#define read_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+
+#define readw_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+
+#define write_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+
+#define writew_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+
+#define un_lock(fd, offset, whence, len) \
+	lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
+
+pid_t lock_test(int, int, off_t, int, off_t);
+
+#define is_read_lockable(fd, offset, whence, len) \
+	(lock_test((fd), F_RDLCK, (offset), (whence), (len)) == 0)
+
+#define is_write_lockable(fd, offset, whence, len) \
+	(lock_test((fd), F_WRLCK, (offset), (whence), (len)) == 0)
+
+// 标准错误函数
+void err_msg(const char *, ...);
+void err_dump(const char *, ...) __attribute__((noreturn));
 void err_quit(const char *, ...) __attribute__((noreturn));
+void err_cont(int, const char *, ...);
+void err_exit(int, const char *, ...) __attribute__((noreturn));
+void err_ret(const char *, ...);
 void err_sys(const char *, ...) __attribute__((noreturn));
+
+// 日志打印函数
+void log_msg(const char *, ...);
+void log_open(const char *, int, int);
+void log_quit(const char *, ...);
+__attribute__((noreturn));
+void log_ret(const char *, ...);
+void log_sys(const char *, ...) __attribute__((noreturn));
+void log_exit(const char *, ...) __attribute__((noreturn));
+
+// 进程相关的函数宏
+void TELL_WAIT(void);
+void TELL_PARENT(pid_t);
+void TELL_CHILD(pid_t);
+void WAIT_PARENT(void);
+void WAIT_CHILD(void);
 
 #endif // APUE_H
