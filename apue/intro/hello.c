4df1ffc2263d128bd968f8d9ae33e5044a2ad6f6
wuyh <wuyhyh@gmail.com>
2024-05-26 10:57:24 -0500

    get process pid

---
+++ apue/intro/hello.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *
+ * Name: hello.c - 打印进程ID
+ * Created on 2024/05/26
+ * Copyright (C) 2022 - 2024, wyh.
+ *
+ *****************************************************************************/
+
+#include "apue.h"
+
+int main(int argc, char argv[])
+{
+	printf("hello from process ID %ld\n", (long)getpid());
+	exit(0)
+}
