eb9e4f0b12f0153e03fc5507f49fadd1caebc929
wuyhyh <wuyhyh@gmail.com>
2024-06-18 21:03:48 +0800

    refactor dir

--- jyyyyds/tar/toybox-0.1.0/toynet.h
+++
@@ -1,9 +0,0 @@
-// Included after toys.h, for network stuff.  Some build environments
-// don't include network support, so we shouldn't include it unless we're
-// going to build it.
-
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <poll.h>
