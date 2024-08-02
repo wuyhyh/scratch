56dda7d519270b7a904b5bc00874d5d827884b09
wuyhyh <wuyhyh@gmail.com>
2024-08-02 23:47:28 +0800

    build scull

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 # 用于linux kernel developing的根Makefile
 
-obj-m := my_module.o
+obj-m := hello.o
+obj-m := scull.o
 
 KDIR := $(PWD)/linux-6.8.5
 
 all:
-	make -C $(KDIR) M=$(PWD)/drivers/hello_module hello
-	make -C $(KDIR) M=$(PWD)/drivers/scull scull
+	make -C $(KDIR) M=$(PWD)/drivers/hello_module module
+	make -C $(KDIR) M=$(PWD)/drivers/scull module
 
 clean:
 	make -C $(KDIR) M=$(PWD)/drivers/hello_module clean
--- drivers/scull/scull.c
+++ drivers/scull/scull.c
@@ -9,22 +9,23 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("wyh");
 MODULE_DESCRIPTION("A simple Hello World LKM");
 MODULE_VERSION("0.1");
 
-static int __init hello_init(void)
+static int __init scull_init(void)
 {
-	printk(KERN_INFO "Hello, World!\n");
+	printk(KERN_INFO "Hello, scull!\n");
 	return 0;
 }
 
-static void __exit hello_exit(void)
+static void __exit scull_exit(void)
 {
-	printk(KERN_INFO "Goodbye, World!\n");
+	printk(KERN_INFO "Goodbye, scull!\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(scull_init);
+module_exit(scull_exit);
