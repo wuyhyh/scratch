a7a461baf4349779237e668e1890434738874113
wuyhyh <wuyhyh@gmail.com>
2024-08-04 21:13:09 +0800

    fix compiling bugs

--- drivers/scull/scull.c
+++ drivers/scull/scull.c
@@ -33,13 +33,28 @@ int scull_nr_devs = SCULL_NR_DEVS;
 
 struct scull_dev *scull_devices;
 
+// 函数的声明和定义
+int scull_open(struct inode *inode, struct file *filp);
+int scull_release(struct inode *inode, struct file *filp);
+int scull_trim(struct scull_dev *dev);
+loff_t scull_llseek(struct file *filp, loff_t off, int whence);
+long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+struct scull_qset *scull_follow(struct scull_dev *dev, int n);
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+		   loff_t *f_pos);
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
+		    loff_t *f_pos);
+static void scull_cleanup_module(void);
+static int __init scull_init(void);
+static void __exit scull_exit(void);
+
 // 设备方法
 struct file_operations scull_fops = {
 	.owner = THIS_MODULE,
 	.llseek = scull_llseek,
 	.read = scull_read,
 	.write = scull_write,
-	.ioctl = scull_ioctl,
+	.unlocked_ioctl = scull_ioctl,
 	.open = scull_open,
 	.release = scull_release,
 };
@@ -187,6 +202,7 @@ ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
 	struct scull_dev *dev = filp->private_data;
 	struct scull_qset *dptr;
 	int quantum = dev->quantum;
+	int qset = dev->qset;
 	int itemsize = quantum * qset;
 	int item, s_pos, q_pos, rest;
 	ssize_t retval = 0;
@@ -211,7 +227,7 @@ ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
 
 	dptr = scull_follow(dev, item);
 
-	if (dptr == NULL || !dptr->data || !dptr.data[s_pos]) {
+	if (dptr == NULL || !dptr->data || !dptr->data[s_pos]) {
 		goto out;
 	}
 
@@ -220,7 +236,7 @@ ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
 	}
 
 	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
-		re_val = -EFAULT;
+		retval = -EFAULT;
 		goto out;
 	}
 	*f_pos += count;
@@ -274,7 +290,7 @@ ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
 	}
 
 	if (copy_from_user(dptr->data[s_pos] + q_pos, buf, count)) {
-		reval = -EFAULT;
+		retval = -EFAULT;
 		goto out;
 	}
 	*f_pos += count;
@@ -296,17 +312,17 @@ static void scull_cleanup_module(void)
 	int i;
 	dev_t devno = MKDEV(scull_major, scull_minor);
 
-	if (scull_dev) {
-		for (int i = 0; i < scull_nr_devs; ++i) {
-			scull_trim(scull_dev);
-			cdev_del(scull_dev->cdev);
+	if (scull_devices) {
+		for (i = 0; i < scull_nr_devs; ++i) {
+			scull_trim(scull_devices+i);
+			cdev_del(&scull_devices[i].cdev);
 		}
-		kfree(scull_dev);
+		kfree(scull_devices);
 	}
 
-	unregister_chrdev_region(devno);
+	unregister_chrdev_region(devno, scull_nr_devs);
 
-	printk(KERN_INFO "Goodbye, scull_module!\n");
+	// TODO: call the cleanup functions for friend devices
 }
 
 // 初始化和清理代码
@@ -317,10 +333,10 @@ static int __init scull_init(void)
 
 	// 动态分配主设备号
 	result = alloc_chrdev_region(&dev, scull_minor, 1, "scull");
-	sc_major = MAJOR(dev);
+	scull_major = MAJOR(dev);
 
 	if (result < 0) {
-		printk(KERNW_WARN, "scull: can't get major number\n");
+		printk(KERN_WARNING, "scull: can't get major number\n");
 		return result;
 	}
 
