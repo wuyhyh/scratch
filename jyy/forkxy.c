9e4fcdbf8bb64494c214a8f9e4f1d1a962715583
wuyhyh <wuyhyh@gmail.com>
2024-08-13 22:28:48 +0800

    jyy fork

---
+++ jyy/forkxy.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *
+ * Name: forkxy.c - Description
+ * Created on 2024/08/13
+ * Copyright (C) 2022 - 2024, wyh.
+ *
+ *****************************************************************************/
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+	pid_t x = fork();
+	pid_t y = fork();
+	printf("%d %d\n", x, y);
+	return 0;
+}
